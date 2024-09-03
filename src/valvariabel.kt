c39fff281fc09889bce1b0c68b8c0edb01bf5355
MUHAMMADALIEFF <muhammad.alief.015373828@gmail.com>
2024-09-03 13:24:31 +0700

    add valvariabel.kt

---
+++ src/valvariabel.kt
@@ -0,0 +1,11 @@
+
+
+fun main(args: Array<String>) {
+    var valVariabel: Int
+    valVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+valVariabel)
+    valVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = "+valVariabel)
+    var phi = 3.14
+    phi = 2.5
+}
